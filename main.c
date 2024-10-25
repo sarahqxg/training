72552a6c24f809ff7da8f6df303c40c710aa922e
sarahqxg <yingqian48@gmail.com>
2024-10-25 17:26:27 +0800

    Create main.c
    this is a test file :print hello world

---
+++ main.c
@@ -0,0 +1,16 @@
+//
+//  main.c
+//  111
+//
+//  Created by 钱小狗 on 25/10/2024.
+//
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, const char * argv[]) {
+
+    printf("Hello, World!\n");
+    
+    return 0;
+}
